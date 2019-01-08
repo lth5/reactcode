84468e312f6f7fe5fd3eae42c51d54cfcbe634b6
liuteng <liuteng@xin.com>
2019-01-08 15:27:30 +0800

    temp

---
+++ source/container/App.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class App extends Component {
+    static propTypes = {}
+    constructor(props) {
+        super(props);
+        this.state = {
+          
+        };
+
+    }
+  
+    componentWillMount() {
+    
+    }
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+export default App;
