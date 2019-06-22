24fc640aefdcb8179e222080a7f735d6116edb33
liuteng <liuteng@xin.com>
2019-06-22 11:56:54 +0800

    无react 使用 rucuex

--- source/container/App.jsx
+++ source/container/App.jsx
@@ -1,23 +1,27 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 
-class App extends Component {
-    static propTypes = {}
-    constructor(props) {
-        super(props);
-        this.state = {
-          
-        };
-
-    }
-  
-    componentWillMount() {
-    
-    }
-    render() {
-        return (
-            <div></div>
-        );
+        
+import {createStore} from 'redux';
+var reducer = (state = 0, action) => {
+    if (!action) return 1;
+    console.log(action);
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
     }
 }
-export default App;
+var store = createStore(reducer);
+store.subscribe(function () {
+    document.querySelector('#counter').innerHTML = store.getState();
+});
+
+document.querySelector('#addBtn').addEventListener('click', function () {
+    store.dispatch({type: 'INCREMENT'});
+});
+document.querySelector('#minusBtn').addEventListener('click', function () {
+    store.dispatch({type: 'DECREMENT'});
+});
+                    
--- source/index.js
+++ source/index.js
@@ -1,30 +1,69 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './container/App.jsx';
-let datalist = [
-    {
-        id: 1,
-        title: '看你不知道的js下',
-        status: 'todo',
-        desc: 'adadadadad',
-        tasks:[]
-    },
-    {
-        id: 2,
-        title: '看你不知道的js中',
-        status: 'doing',
-        desc: 'adadadadad',
-        tasks:[]
-    },
-    {
-        id: 3,
-        title: '看你不知道的js上',
-        status: 'done',
-        desc: 'adadadadad',
-        tasks:[]
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './container/App.jsx';
+// import {createStore} from 'redux'
+// var reducer = (state = 0, action) => {
+//     if (!action) return state;
+   
+//     switch (action.type) {
+//         case 'INCREMENT':
+//             return state + 1;
+//         case 'DECREMENT':
+//             return state - 1;
+//         default:
+//             return state;
+//     }
+// }
+// const store=createStore(reducer);
+
+// ReactDOM.render(
+// 	<App  />,
+// 	document.getElementById('root')
+// )
+// function createStore(reducer) {
+//     var state;
+//     var listeners = [];
+//     var getState = () => state;
+//     var dispatch = (action) => {
+//         state = reducer(state, action);
+//         listeners.forEach(l=>l());
+//     }
+//     var subscribe = (listener) => {
+//         listeners.push(listener);
+//         return () => {
+//             listeners = listeners.filter((l) => l != listener)
+//         }
+//     }
+//     dispatch();
+//     return {
+//         getState, dispatch, subscribe
+//     }
+// }
+        
+import {createStore} from 'redux';
+var reducer = (state = 0, action) => {
+    if (!action) return state;
+    console.log(action);
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
     }
-]
-ReactDOM.render(
-	<App cards={datalist} />,
-	document.getElementById('root')
-)
+}
+var store = createStore(reducer);
+var a = store.subscribe(function () {
+    document.querySelector('#counter').innerHTML = store.getState();
+});
+
+
+document.querySelector('#addBtn').addEventListener('click', function () {
+    console.log(a());
+    store.dispatch({type: 'INCREMENT'});
+});
+document.querySelector('#minusBtn').addEventListener('click', function () {
+    store.dispatch({type: 'DECREMENT'});
+});
+                    
